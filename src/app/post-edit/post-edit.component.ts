525adf329c2495717056f88e85ddba36e313d129
Daniel Lynch <danielwewo@gmail.com>
2023-11-18 19:03:44 -0800

    progress on post edit

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,19 +1,47 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-post-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './post-edit.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostEditComponent {
   postForm = new FormGroup({
-    title: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(255),
-    ]),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(512)],
+    }),
+    content: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(4000000)],
+    }),
   });
+
+  get title(): FormControl<string> {
+    return this.postForm.controls.title;
+  }
+
+  get content(): FormControl<string> {
+    return this.postForm.controls.content;
+  }
+
+  submitPost(): void {}
 }
